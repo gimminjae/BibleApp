4a175f70f49ad1cb087f615811f14d666648a9d9
gimminjae <min356812@naver.com>
2023-02-23 22:35:58 +0900

    commit

---
+++ fo/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Hello Bible App User</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+</style>
