6d458590f2d4f039c997366cc017a60e0ea569d7
gimminjae <min356812@naver.com>
2023-02-25 03:25:23 +0900

    클릭시 읽은 수 증가, 백엔드에도 적용 필요

--- fo/src/components/AppCard.vue
+++ fo/src/components/AppCard.vue
@@ -7,7 +7,7 @@
       <blockquote class="blockquote mb-0 d-flex flex-wrap gap-2 justify-content-start">
         <div v-for="(bibleCount, index) in readList" :key="bibleCount">
           <button type="button" :class="readCount(bibleCount)"
-                  style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+                  style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;" @click="bibleCountPlus(index, bibleCount)">
             {{ index + 1 }}
             <span v-if="bibleCount != 0" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-info">
               {{ bibleCount }}
@@ -34,6 +34,10 @@ export default {
     }
   },
   methods: {
+    bibleCountPlus(index, bibleCount) {
+      this.readList[index]++;
+      console.log(index, bibleCount);
+    },
     readCount(count) {
       if(count == 0) {
         return "btn btn-outline-info position-relative";
